381878d63023d605200970ceeaa2cb0bfad166c3
Юрий Лихачёв <somes@live.ru>
2018-05-10 06:30:01 +0300

    Final ver.

---
+++ FuncMinimum/Program.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * Лихачев Юра
+ * 2. *Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата. 
+ * Сделать меню с различными функциями и представьте пользователю выбор для какой функции и на каком отрезке находить минимум. 
+ * Используйте массив делегатов.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FuncMinimum
+{
+    class Program
+    {
+        static class MenuItemsEnum
+        {
+            public const int First = 0;
+            public const int Second = 1;
+        }
+
+        public delegate double MyDelegate(double dNumber);
+
+        public static double F1(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+        public static double F2(double x)
+        {
+            return x * 2;
+        }
+
+        public static void SaveFunc(string fileName, MyDelegate myDelegate, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(myDelegate(x));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+
+
+        // Method selecting menu, returns menu item index.
+        public static int GetMenuIndex()
+        {
+            string sMenuItem1 = "Вычислить значение x*x-50*x+10";
+            string sMenuItem2 = "Вычислить значение x*2";
+            int iEnteredValue = -1;
+
+            Dictionary<int, string> menuDictionary = new Dictionary<int, string>()
+            {
+                { MenuItemsEnum.First, sMenuItem1 },
+                { MenuItemsEnum.Second, sMenuItem2 }
+            };
+
+            Console.WriteLine("Пожалуйста, введите номер варианта для выбора.");
+            Console.WriteLine("{0,2} : {1}", MenuItemsEnum.First, sMenuItem1);
+            Console.WriteLine("{0,2} : {1}", MenuItemsEnum.Second, sMenuItem2);
+
+            while (!menuDictionary.ContainsKey(iEnteredValue))
+                int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out iEnteredValue);
+            return iEnteredValue;
+        }
+
+        static void Main(string[] args)
+        {
+            MyDelegate[] aMyDelegates = new MyDelegate[2];
+
+            aMyDelegates[MenuItemsEnum.First] = F1;
+            aMyDelegates[MenuItemsEnum.Second] = F2;
+
+            int iIndex = GetMenuIndex();
+
+            SaveFunc("data.bin", aMyDelegates[iIndex], -100, 100, 0.5);
+
+            Console.WriteLine(Load("data.bin"));
+
+
+            Console.WriteLine("\nНажмите любую клавишу для выхода.");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ StudentsCSV/Program.cs
@@ -0,0 +1,149 @@
+﻿/*
+ * Лихачев Юра
+ * 3. Подсчитать количество студентов:
+ * а) учащихся на 5 и 6 курсах;
+ * б)подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся(частотный массив);
+ * в) отсортировать список по возрасту студента;
+ * г) *отсортировать список по курсу и возрасту студента.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StudentsCSV
+{
+    class Program
+    {
+        enum FileFormat
+        {
+            Name = 0,
+            LastName = 1,
+            Univeristy = 2,
+            Faculty = 3,
+            Department = 4,
+            Age = 5,
+            Course = 6,
+            Group = 7,
+            City = 8
+        }
+
+        enum Courses
+        {
+            First = 1,
+            Second = 2,
+            Third  = 3,
+            Fourth = 4,
+            Fifth = 5,
+            Sixth = 6
+        }
+
+        // Checking student age
+        static bool AgeCheck(string[] asInputData, int iAgeMin, int iAgeMax)
+        {
+            int iAge = int.Parse(asInputData[(int)FileFormat.Age]);
+            return (iAge >= iAgeMin) && (iAge <= iAgeMax);
+        }
+
+        // Comparing by course and age
+        static int CSVComparer(string[] asInputData1, string[] asInputData2)
+        {
+            int iCourse1 = int.Parse(asInputData1[(int)FileFormat.Course]);
+            int iCourse2 = int.Parse(asInputData2[(int)FileFormat.Course]);
+
+            int iAge1 = int.Parse(asInputData1[(int)FileFormat.Age]);
+            int iAge2 = int.Parse(asInputData2[(int)FileFormat.Age]);
+
+            if (iCourse1 > iCourse2)
+            {
+                return 1;
+            }
+            else if (iCourse1 == iCourse2)
+            {
+                if (iAge1 > iAge2) { return 1; }
+                else if (iAge1 == iAge2) { return 0; }
+                else { return -1; }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            FileStream fileStreamStudentsInput = new FileStream("..\\..\\students_1.csv", FileMode.Open, FileAccess.Read);
+            StreamReader streamReader = new StreamReader(fileStreamStudentsInput);
+
+            FileStream fileStreamStudentsSortedOutput = new FileStream("..\\..\\students_2.csv", FileMode.Create, FileAccess.Write);
+            StreamWriter streamWriter = new StreamWriter(fileStreamStudentsSortedOutput);
+
+            // Array with courses we are interesting in
+            int[] aiCoursesShortScope = new int[2] { (int)Courses.Fifth, (int)Courses.Sixth };
+            int iStudentsCounter1 = 0;
+
+            // Frequency array
+            int[] aiCourses = new int[7];
+            for (int iCounter1 = 0; iCounter1 < aiCourses.Length; iCounter1++)
+                aiCourses[iCounter1] = 0;
+
+            const int MINIMUM_AGE = 18;
+            const int MAXIMUM_AGE = 20;
+
+            // For sorting purposes
+            List<string[]> lsRows = new List<string[]>();
+
+            string[] sTemp;
+
+            while(!streamReader.EndOfStream)
+            {
+                sTemp = streamReader.ReadLine().Split(';');
+                
+                // Is student from our courses scope?
+                if (aiCoursesShortScope.Contains(int.Parse(sTemp[(int)FileFormat.Course])))
+                    iStudentsCounter1++;
+
+                if (AgeCheck(sTemp, MINIMUM_AGE, MAXIMUM_AGE))
+                    aiCourses[int.Parse(sTemp[(int)FileFormat.Course])]++;
+
+                lsRows.Add(sTemp);
+            }
+            streamReader.Close();
+            fileStreamStudentsInput.Close();
+
+            // List sorting with custom comparer
+            lsRows.Sort(CSVComparer);
+
+            // Writing sorted list to file 
+            foreach (string[] asRow in lsRows){
+                streamWriter.WriteLine(String.Join(";", asRow));
+            }
+            streamWriter.Close();
+            fileStreamStudentsSortedOutput.Close();
+
+            // Output part
+            Console.Title = "Some fun with CSV 1.0";
+            Console.WriteLine("Данное приложение обрабатывает CSV файл {0} заранее определенным образом.", fileStreamStudentsInput.Name);
+            Console.WriteLine("Количество студентов, обучающихся на {0} и {1} курсах:", (int)Courses.Fifth, (int)Courses.Sixth);
+            Console.WriteLine(iStudentsCounter1);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Подсчет количества студентов, в возрасте от {0} до {1}, обучающихся на различных курсах:", MINIMUM_AGE, MAXIMUM_AGE);
+            for (int iCounter1 = 1; iCounter1 < aiCourses.Length; iCounter1++)
+            {
+                Console.WriteLine("Курс {0} : {1}", iCounter1, aiCourses[iCounter1]);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Перечень студентов, отсортированный по курсу и возрасту студентов, записан в файл {0}\n", fileStreamStudentsSortedOutput.Name);
+
+            Console.WriteLine("Нажмите любую клавишу для выхода.");
+            Console.ReadKey(true);
+        }
+    }
+}
